675476a7a4a9ae7052367b427f9a92f5159c10d4
Niclas Hillgemann <niclas-hillgemann@niclass-mbp.student.berlin.sae>
2019-02-20 16:54:48 +0100

    neuer versuch

--- user.php
+++ user.php
@@ -9,16 +9,14 @@ class user {
    public $password;
    public $height;
    public $weight;
-   public $daily_water;
 
-   public function __construct($name, $email, $password, $username, $height, $weight, $daily_water){
+   public function __construct($name, $email, $password, $username, $height, $weight){
           $this->name = $name;
           $this->email = $email;
           $this->username = $username;
           $this->password = $password;
           $this->height = $height;
           $this->weight = $weight;
-          $this->daily_water = $daily_water;
    }
 }
 ?>
